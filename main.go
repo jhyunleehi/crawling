b69c9cf8862765e0762e1b4f135a2b95d7f38098
jhyunleehi <jhyunleehi@naver.com>
2023-06-02 20:36:46 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,779 @@
+package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}package main
+
+
+import (
+
+
+    "crawling/craw"
+
+
+    "os"
+
+
+    "time"
+
+
+    nested "github.com/antonfisher/nested-logrus-formatter"
+
+
+    log "github.com/sirupsen/logrus"
+
+
+)
+
+
+func init() {
+
+
+    log.SetLevel(log.DebugLevel)
+
+
+    log.SetReportCaller(true)
+
+
+    log.SetFormatter(&nested.Formatter{
+
+
+        HideKeys:        true,
+
+
+        TimestampFormat: time.RFC3339,
+
+
+        NoColors:        true,
+
+
+    })
+
+
+}
+
+
+func main() {
+
+
+    var product string
+
+
+    if len(os.Args) < 2 {
+
+
+        //panic("에러: 2개 미만의 argument")
+
+
+        product = "B084RGZ3P7"
+
+
+    } else {
+
+
+        product = os.Args[1]
+
+
+    }
+
+
+    //"B084RGZ3P7"
+
+
+    host := "https://www.amazon.com"
+
+
+    path := "/product-reviews/" + product
+
+
+    c := craw.NewCraw(host, path, product)
+
+
+    err := c.GetWebData()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+    err = c.WriteToFile()
+
+
+    if err != nil {
+
+
+        log.Error(err)
+
+
+    }
+
+
+}
+
